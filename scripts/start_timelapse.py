021d0be475035fc3704c1457681206bb99342987
SamuelClucas <samuelclucas2001@gmail.com>
2024-09-10 12:25:38 +0100

    Fixing import statements

--- scripts/start_timelapse.py
+++ scripts/start_timelapse.py
@@ -16,7 +16,7 @@ Usage Examples:
     Basic use:
         
 """
-from timelapsEr import *
+from timelapsEr import camera_controller, get_date, neopixel_controller 
 
 import argparse
 import sys, os, time
@@ -50,7 +50,7 @@ elif args.units == 'd':
 if args.samples != 0 and args.duration != 0: # prevents division by 0 and 0 not divisible errors
     interval = args.duration / args.samples
 
-saveLocation = os.path.join(args.path, args.name, tl.get_today(), tl.get_now())
+saveLocation = os.path.join(args.path, args.name, get_date.get_today(), get_date.get_now())
 
 if not os.path.exists(saveLocation):
     # If current path does not exist in specified save file path, create it
@@ -58,14 +58,14 @@ if not os.path.exists(saveLocation):
 
 def timelapse(saveLocation):        
     # instantiate timelapsEr objects
-    camera = CameraController(saveLocation) # configures camera module
-    light = light()
+    camera = camera_controller.CameraController(saveLocation) # configures camera module
+    light = neopixel_controller.NeopixelController()
 
     # imaging loop
     for timepoint in range(args.samples):
         light.on()
 
-        print(f"Taking image {timepoint+1} at {get_now()}")
+        print(f"Taking image {timepoint+1} at {get_date.get_now()}")
         camera.capture_image(timepoint+1)
 
         light.off()
--- timelapsEr/__init__.py
+++
@@ -1,3 +0,0 @@
-from .camera_controller import CameraController
-from .neopixel_controller import NeopixelController as light
-from .get_date import get_now, get_today
