68d18d0ae55ed74df79a6e1872119b109216c8c8
SamuelClucas <samuelclucas2001@gmail.com>
2024-09-10 12:52:32 +0100

    Fixing import statements

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='timelapsEr',
-    packages=find_packages()
+    packages=['timelapsEr']
+    scripts=['start_timelapse.py']
 )
