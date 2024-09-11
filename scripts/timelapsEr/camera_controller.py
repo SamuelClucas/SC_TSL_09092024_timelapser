c91d789e7bc1442c5e464b2b301dc855fcdbf86e
SamuelClucas <samuelclucas2001@gmail.com>
2024-09-11 16:12:03 +0100

    Simple sharpness algo print

--- scripts/timelapsEr/camera_controller.py
+++ scripts/timelapsEr/camera_controller.py
@@ -1,6 +1,7 @@
 from picamera2 import Picamera2, Preview
 from libcamera import controls
 from timelapsEr import get_date
+import numpy as np
 '''
 See documentation here: https://datasheets.raspberrypi.com/camera/picamera2-manual.pdf
 
@@ -16,13 +17,11 @@ class CameraController:
         
         self.config = self.picam2.create_still_configuration() 
         
-        #self.picam2.set_controls({"AeEnable": True, "AwbEnable": True, "FrameRate": 1.0, "AfMode": controls.AfModeEnum.Continuous}) # from Alison's camera_project
         self.picam2.set_controls({"AfMode": controls.AfModeEnum.Auto, "AeEnable": True, "AwbEnable": True})
         
         self.picam2.options["quality"] = 95
         print(self.config["main"])
         
-        #self.config.align() # optimises stream size alignment
         self.picam2.configure(self.config)
         
         self.picam2.start()
@@ -32,9 +31,14 @@ class CameraController:
     def capture_image(self, timepoint):
         # capture image logic 
 
-        #imageArray = self.picam2.capture_array("main")  \ if in future you want to pass the images directly to opencv as arrays, see here*
-        #np.shape(imageArray)    
-        #                          
+        imageArray = self.picam2.capture_array("main")  
+        np.shape(imageArray)    
+
+        gy, gx = np.gradient(imageArray)
+        gnorm = np.sqrt(gx**2 + gy**2)
+        sharpness = np.average(gnorm)
+        print(sharpness)
+                               
         success = self.picam2.autofocus_cycle()
         request = self.picam2.capture_request(flush=True)
         metadata = request.get_metadata()
