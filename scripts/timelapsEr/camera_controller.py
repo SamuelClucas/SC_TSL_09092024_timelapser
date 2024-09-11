ce55b1498d557d00157fd896d24a2f109fdca534
SamuelClucas <samuelclucas2001@gmail.com>
2024-09-11 14:16:00 +0100

    AFstate 0 lenspoisiton 0: fixed missing bracket

--- scripts/timelapsEr/camera_controller.py
+++ scripts/timelapsEr/camera_controller.py
@@ -17,7 +17,7 @@ class CameraController:
         self.config = self.picam2.create_still_configuration() 
         
         #self.picam2.set_controls({"AeEnable": True, "AwbEnable": True, "FrameRate": 1.0, "AfMode": controls.AfModeEnum.Continuous}) # from Alison's camera_project
-        self.picam2.set_controls({"AfMode": controls.AfModeEnum.Manual, "LensPosition": 0.0}
+        self.picam2.set_controls({"AfMode": controls.AfModeEnum.Manual, "LensPosition": 0.0})
         self.picam2.options["quality"] = 95
         print(self.config["main"])
         
