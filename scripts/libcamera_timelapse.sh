7d7c5ce5ff87d7eca9ea102e280308d00cb167ec
SamuelClucas <samuelclucas2001@gmail.com>
2024-09-12 15:04:11 +0100

    fixing savefilename counter

--- scripts/libcamera_timelapse.sh
+++ scripts/libcamera_timelapse.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 filename="$($capture_%0d.png" "$1")"
 
-libcamera-still -n true %focus --autofocus-mode=continuous --encoding png -o $1/$filename  
+libcamera-still -n true %focus --autofocus-mode=continuous --encoding png -o $2/$filename  
